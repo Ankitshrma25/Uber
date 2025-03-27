226df137f18b0854f0faa692c9a88728cc3138f2
Ankitshrma25 <ankit302290@gmail.com>
2025-03-27 21:24:39 +0530

    Added LocationSearchPanel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react'
 import {useGSAP} from '@gsap/react'
 import { gsap } from 'gsap'
+import 'remixicon/fonts/remixicon.css'
+
 
 const Home = () => {
 
@@ -8,21 +10,32 @@ const Home = () => {
   const [destination, setdestination] = useState('')
   const [panelOpen, setPanelOpen] = useState(false)
   const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
 
   const submitHandler = (e) => {
       e.preventDefault()
 
   }
 
-  useGSAP(() => {
-    if (panelOpen && panelRef.current) {
-      gsap.fromTo(
-        panelRef.current, 
-        { height: '0%' }, // Initial state
-        { height: '70%', duration: 0.5, ease: 'power2.out' } // Final state
-      );
+  useGSAP(function () {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '70%',
+        opacity: 1
+      })
+      gsap.to(panelCloseRef.current, {
+        opacity: 1
+      })
+    } else {
+      gsap.to(panelRef.current, {
+        height: '0%',
+        opacity: 0
+      })
+      gsap.to(panelCloseRef.current, {
+        opacity: 0
+      })
     }
-  }, [panelOpen]);
+  }, [panelOpen])
 
   return (
     <div className='h-screen relative'>
@@ -32,7 +45,12 @@ const Home = () => {
         <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
       </div>
       <div className=' flex flex-col justify-end h-screen absolute top-0 w-full '>
-        <div className='h-[30%] p-5 bg-white relative'>
+        <div className='h-[30%] p-6 bg-white relative'>
+          <h5 ref={panelCloseRef} onClick={()=>{
+            setPanelOpen(false)
+            }} className='absolute opacity-0 te top-2 left-1/2 -translate-x-1/2'>
+          <i className="ri-arrow-down-wide-line"></i>
+          </h5>
           <h4 className='text-2xl font-semibold'>Find a trip</h4>
           <form onSubmit={(e) => {
             submitHandler(e)
@@ -62,7 +80,7 @@ const Home = () => {
             placeholder='Enter your destination' />
           </form>
         </div>
-        <div ref={panelRef} className=' bg-red-500 h-0 '>
+        <div ref={panelRef} className=' bg-red-500 h-0 opacity-0 '>
 
         </div>
       </div>
