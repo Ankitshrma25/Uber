1b7cde5bae5fc438201ba66294bbcf364b3abbb8
Ankitshrma25 <ankit302290@gmail.com>
2025-03-31 23:12:26 +0530

    Made changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -94,7 +94,7 @@ const Home = () => {
         <div className='flex w-full p-1 mb-2 border-2 border-black rounded-xl items-center justify-between '>
           <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_300,w_533/v1646663215/assets/6e/e50c1b-2174-4c97-83a1-bfd4544689d0/original/uberX.png" alt="" />
 
-          <div className='w1/2 ml-2'>
+          <div className='w-1/2 ml-2'>
             <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-fill"></i>4</span></h4>
             <h5>2 mins away</h5>
             <p className='font-normal text-xs text-gray-600'>Affordable, compact, and eco-friendly rides</p>
@@ -105,7 +105,7 @@ const Home = () => {
         <div className='flex w-full p-1 mb-2 border-2 border-black rounded-xl items-center justify-between '>
           <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_1152/v1649230978/assets/a2/553a18-2f77-4722-a4ba-f736f4cb405e/original/Uber_Moto_Orange_558x372_pixels_Desktop.png" alt="" />
 
-          <div className='w1/2 ml-2'>
+          <div className='w-1/2 ml-2'>
             <h4 className='font-medium text-base'>Moto<span><i className="ri-user-fill"></i>1</span></h4>
             <h5>4 mins away</h5>
             <p className='font-normal text-xs text-gray-600'>Affordable, compact, and eco-friendly motercycle rides</p>
@@ -116,12 +116,12 @@ const Home = () => {
         <div className='flex w-full p-1 mb-2 border-2 border-black rounded-xl items-center justify-between '>
           <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_768,w_1152/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
 
-          <div className='w1/2 ml-2'>
+          <div className='w-1/2 ml-2'>
             <h4 className='font-medium text-base'>Auto<span><i className="ri-user-fill"></i>3</span></h4>
-            <h5>5 mins away</h5>
-            <p className='font-normal text-xs text-gray-600'>Affordable, compact, and eco-friendly Auto rides</p>
+            <h5>5 mins away</h5> 
+            <p className='font-normal text-xs text-gray-600'>Affordable Auto rides</p>
           </div>
-          <h2 className='text-xl font-medium'>₹94</h2>
+          <h2 className='text-xl font-base'>₹94</h2>
         </div>
       </div>
 
