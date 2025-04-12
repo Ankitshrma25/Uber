476552530718dc9f7ea379b2f1683c4793f53fc0
Ankitshrma25 <ankit302290@gmail.com>
2025-04-12 22:38:27 +0530

    Completed Route for Ride

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const rideController = require('../controllers/ride.controller');
 
 
 router.post('/create', 
     body('userId').isString().isLength({ min: 24, max: 24 }).withMessage('Invalid user ID'),
     body('pickup').isString().isLength({ min: 3 }).withMessage('Pickup location is required'),
     body('destination').isString().isLength({ min: 3 }).withMessage('Dropoff location is required'),
+    body('vehicleType').isString().isIn(['auto', 'car', 'motorcycle']).withMessage('Invalid vehicle type'),
+    rideController.createRide
 );
 
 
